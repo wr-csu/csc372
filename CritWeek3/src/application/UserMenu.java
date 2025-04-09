c218f44426ee9dbe02e927d0357e801c65c76368
wr-csu <whitney.richards@csuglobal.edu>
2025-04-09 15:41:24 -0400

    commit crit3 updated code

--- CritWeek3/src/application/UserMenu.java
+++ CritWeek3/src/application/UserMenu.java
@@ -1,6 +1,6 @@
 package application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+//many many imports
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -13,32 +13,48 @@ import java.time.format.DateTimeFormatter;
 import java.io.FileWriter; 
 import java.io.IOException;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle; 
 import java.util.Random;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.StackPane;
 import javafx.scene.control.Label;
 
 
 public class UserMenu {
-	
-	
 	public Stage stage;
 	private BorderPane layout;
 	private Scene scene;
 
-
 	public UserMenu(Stage stage) {
 		this.stage = stage;
 		GUI();
-
 	}
-
+	//create orange color
+	public Color makeOrange() {
+    	Random rand = new Random();
+    	int g = rand.nextInt(66, 157);
+    	Color c = Color.rgb(255,g,0);
+		return c;
+	}
+	//creates alerts	
+	public Alert makeAlert(String title, String contentText, String text) {
+	    Alert alert = new Alert(AlertType.INFORMATION);
+	    alert.setTitle(title);
+	    alert.setHeaderText(null);
+	    alert.setContentText(contentText + text);
+	    alert.showAndWait();
+		return alert;	
+	}	
+	
 	private void GUI() {
 		//set layout and create menu
 		MenuBar menuBar = new MenuBar();
-		BorderPane layout = new BorderPane();
+		layout = new BorderPane();
 		layout.setTop(menuBar);
 		Menu fileMenu = new Menu("File");
+		
 		//adding all items to the menu
 		MenuItem currDate = new MenuItem("Current Date");
 		MenuItem logDate = new MenuItem("Log Date");
@@ -47,65 +63,51 @@ public class UserMenu {
 		fileMenu.getItems().addAll(currDate, logDate, orange, exit);
 		menuBar.getMenus().addAll(fileMenu);
 		
+		//date variables
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM uuuu");
+		LocalDate date = LocalDate.now();
+		String text = date.format(formatter);
+		
+		
 		//action event for current date
-		currDate.setOnAction(new EventHandler<ActionEvent>()    {
-            @Override
-            public void handle(ActionEvent event) {
-            	LocalDate date = LocalDate.now();
-            	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM uuuu");
-            	String text = date.format(formatter);
-            	LocalDate parsedDate = LocalDate.parse(text, formatter);
-                Alert alert = new Alert(AlertType.INFORMATION);
-                alert.setTitle("Current Date");
-                alert.setHeaderText(null);
-                alert.setContentText("Today: " + text);
-                alert.showAndWait();
-                }
-        });		
-		// action event for logging date
-		logDate.setOnAction(new EventHandler<ActionEvent>()    {
-            @Override
-            public void handle(ActionEvent event) {
-                try {
-                	LocalDate date = LocalDate.now();
-                	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM uuuu");
-                	String text = date.format(formatter);
-                	LocalDate parsedDate = LocalDate.parse(text, formatter);
-                    FileWriter newDate = new FileWriter("log.txt");
-                    newDate.write(text);
-                    newDate.close();
-                    Alert alert = new Alert(AlertType.INFORMATION);
-                    alert.setTitle("Info");
-                    alert.setHeaderText(null);
-                    alert.setContentText("Logged today's date");
-                    alert.showAndWait();
-                  } catch (IOException e) {
-                    System.out.println("ERROR");
-                    e.printStackTrace();            	
-            }}});
+		currDate.setOnAction(event -> {			
+			makeAlert("Current Date", "Today: ", text);			
+		});	
+		
+		// action event for logging date				
+		logDate.setOnAction(event -> {
+            try {          	         	        
+          	    try (FileWriter newDate = new FileWriter("log.txt",true)){
+          	    	newDate.write(text);
+          	    	newDate.write(System.getProperty( "line.separator" ));
+          	    }  
+          	    
+          	    makeAlert("Info", "Logged today's date", text);
+          	    	         	   
+             } catch (IOException e) {
+                System.out.println("File Not Found");
+                e.printStackTrace();            	
+         }});		
 		//action event for orange color changer
-		orange.setOnAction(new EventHandler<ActionEvent>()    {
-            @Override
-            public void handle(ActionEvent event) {
-            	Random rand = new Random();
-            	int g = rand.nextInt(66, 157);
-            	Color c = Color.rgb(255,g,0);
+		orange.setOnAction(event -> {
+			//reset layout per event click
+			layout.setCenter(null);
+		    layout.setBottom(null);
+		    
+		    //new layout to handle orange color setting
+			StackPane root = new StackPane();
+			root.setBackground(new Background(new BackgroundFill(makeOrange(), CornerRadii.EMPTY, Insets.EMPTY)));
+			layout.setCenter(root);
+			
+			//label to output orange color
+			Label label = new Label("This Orange is: " + makeOrange()); 
+        	BorderPane.setAlignment(label, javafx.geometry.Pos.BOTTOM_CENTER);
+			layout.setBottom(label); 	
+        });	
+     	//action even to close out window
+		exit.setOnAction(event -> {	System.exit(0);});
 
-            	Rectangle rect = new Rectangle(350,350, c);
-            	BorderPane.setAlignment(rect, javafx.geometry.Pos.CENTER);
-            	layout.setCenter(rect);
-            	Label label = new Label("This Orange is: " + c);  
-            	BorderPane.setAlignment(label, javafx.geometry.Pos.BOTTOM_CENTER);
-            	layout.setBottom(label);      	
-            }});
-
-		//action even to close out window
-		exit.setOnAction(new EventHandler<ActionEvent>()    {
-            @Override
-            public void handle(ActionEvent event) {		
-            	System.exit(0);
-		}});
-
+		
 		//set scene
 		scene = new Scene(layout, 400, 400);
 		stage.setTitle("User Menu: Orange");
